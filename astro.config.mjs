fbd76eb4af2b7d91f582428bb928a7708abeac97
Anurag Gaur <anuraggaur29@gmail.com>
2026-05-07 22:16:47 +0530

    Clean unused template files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -7,5 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ramneekkaur.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [sitemap(), tailwind(), react()]
 });
